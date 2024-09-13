105afeeaf2642481c91a609c8fb0ab4fd2ee15ae
Dolzhenkov-Nikita <Dolzhenkov.na@gmail.com>
2024-09-13 12:42:12 +0500

    feat(config): published bat-media.php

--- src/MediaEntity/Providers/BatMediaServiceProvider.php
+++ src/MediaEntity/Providers/BatMediaServiceProvider.php
@@ -2,13 +2,13 @@
 
 namespace Batyukovstudio\BatMedia\MediaEntity\Providers;
 
-use Illuminate\Support\ServiceProvider as ParentMainServiceProvider;
+use Illuminate\Support\ServiceProvider;
 
 /**
  * The Main Service Provider of this container.
  * It will be automatically registered by the framework.
  */
-class BatMediaServiceProvider extends ParentMainServiceProvider
+class BatMediaServiceProvider extends ServiceProvider
 {
     public array $serviceProviders = [
         // InternalServiceProviderExample::class,
@@ -18,8 +18,18 @@ class BatMediaServiceProvider extends ParentMainServiceProvider
         // 'Foo' => Bar::class,
     ];
 
+    public function boot(): void
+    {
+        $source = __DIR__ . '/../config/bat-media.php';
+
+        $this->publishes([
+            $source => config_path('bat-media.php')
+        ]);
+    }
+
     public function register(): void
     {
         parent::register();
     }
+
 }
